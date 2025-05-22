13f0d3954c8325eeb0b253095ced51a82570f5ba
winlogon <walker84837@gmail.com>
2025-05-22 10:28:05 +0200

    change to async traversing

--- src/config.rs
+++ src/config.rs
@@ -1,19 +1,20 @@
 use serde_derive::Deserialize;
-///
+
+/// Configuration for extensions to look for
 #[derive(Deserialize)]
 pub struct CodeFileConfig {
     pub code_file_extensions: Vec<CodeFileExtension>,
     pub ignore: IgnoreConfig,
 }
 
-///
+/// Individual extension with file type
 #[derive(Deserialize)]
 pub struct CodeFileExtension {
     pub extension: String,
     pub file_type: String,
 }
 
-///
+/// Files and folders to ignore
 #[derive(Deserialize)]
 pub struct IgnoreConfig {
     pub folders: Vec<String>,
