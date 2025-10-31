095eeded2cb4aa1713b51423417ee1e04eaed1ce
winlogon <walker84837@gmail.com>
2025-10-31 03:43:25 +0100

    feat: add comment patterns to configuration
    Fixes #8

--- src/config.rs
+++ src/config.rs
@@ -5,6 +5,7 @@ use serde_derive::Deserialize;
 pub struct CodeFileConfig {
     pub code_file_extensions: Vec<CodeFileExtension>,
     pub ignore: IgnoreConfig,
+    pub comment_patterns: CommentPatterns,
 }
 
 /// Individual extension with file type
@@ -12,6 +13,14 @@ pub struct CodeFileConfig {
 pub struct CodeFileExtension {
     pub extension: String,
     pub file_type: String,
+}
+
+/// Comment patterns for different languages
+#[derive(Deserialize)]
+pub struct CommentPatterns {
+    pub single_line: Vec<String>,
+    pub multi_line_start: Vec<String>,
+    pub multi_line_end: Vec<String>,
 }
 
 /// Files and folders to ignore
